d10093c085fc65f5c5337765d87d67ce9435a9a4
Mazen <mazen.elgammal92@gmail.com>
2018-07-28 16:48:21 +0200

    updating message part

--- index.php
+++ index.php
@@ -441,69 +441,17 @@ $test=new CsrfToken();
                     <p class="lead">Get in touch,
                         I'm always standing by and eager to help.
                     </p>
+                      <aside class="four columns footer-widgets">
+                        <div class="widget widget_contact">
+                            <h4>Address</h4>
+                                <p class="address">
+                                        <span>Mazen El-Gammal</span><br>
+                                        <span> Cairo, Egypt</span><br>
+                                        <span>mazen.elgammal92@gmail.com</span><br>
+                                </p>
+                        </div>
+                    </aside>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="eight columns">
-                    <!-- form -->
-                    <form action="" method="post" id="contactForm" name="contactForm">
-                        <?php echo $test->generateHiddenField();?>
-<!--                    <input type="hidden" value="<?php //echo $token;?>" id="CSRF_Token" name="CSRF_">-->
-                        <fieldset>
-
-                            <div>
-                                <label for="contactName">Name <span class="required">*</span></label>
-                                <input type="text" value="" size="35" id="contactName" name="contactName">
-                            </div>
-
-                            <div>
-                                <label for="contactEmail">Email <span class="required">*</span></label>
-                                <input type="text" value="" size="35" id="contactEmail" name="contactEmail">
-                            </div>
-
-                            <div>
-                                <label for="contactSubject">Subject</label>
-                                <input type="text" value="" size="35" id="contactSubject" name="contactSubject">
-                            </div>
-
-                            <div>
-                                <label for="contactMessage">Message <span class="required">*</span></label>
-                                <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                            </div>
-
-                            <div>
-                                <button class="submit">Submit</button>
-                                <span id="image-loader">
-                                    <img alt="" src="images/loader.gif">
-                                </span>
-                            </div>
-
-                        </fieldset>
-                    </form> <!-- Form End -->
-
-                    <!-- contact-warning -->
-                    <div id="message-warning"> Error boy</div>
-                    <!-- contact-success -->
-                    <div id="message-success">
-                        <i class="fa fa-check"></i>Your message was sent, thank you!<br>
-                    </div>
-
-                </div>
-
-
-                <aside class="four columns footer-widgets">
-                    <div class="widget widget_contact">
-                        <h4>Address</h4>
-                            <p class="address">
-                                    <span>Mazen El-Gammal</span><br>
-                                    <span> Cairo, Egypt</span><br>
-                                    <span>mazen.elgammal92@gmail.com</span><br>
-                                    <span>admin@mazenelgammal.com</span>
-                            </p>
-                    </div>
-
-                </aside>
             </div>
         </section> <!-- Contact Section End-->
 
